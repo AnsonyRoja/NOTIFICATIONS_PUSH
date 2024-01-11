15037626497c59746c1663d6ce2f22355075d690
AnsonyRoja <ansonyrojas2015@gmail.com>
2024-01-11 14:21:28 -0400

    l

--- src/notifications/notificationPush.js
+++ src/notifications/notificationPush.js
@@ -46,7 +46,7 @@ const checkAndNotifyDocumentsForUser = async (user) => {
 
 
             });
-                console.log("esta es la respuesta",response?.data.WindowTabData.DataSet.DataRow);
+                console.log("esta es la respuesta",response?.data.WindowTabData.DataSet.DataRow.field);
                 // if(response?.data.WindowTabData.DataSet.DataRow === undefined){    
                 //     const documentoUnico = response?.data.WindowTabData.DataSet.DataRow;
 
